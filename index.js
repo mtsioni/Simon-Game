783c4da4b089e3b97fd5c4efd6344f41b2212031
Maria Tsioni <thalia.tsioni@gmail.com>
2023-10-25 12:37:56 +0300

    color sequence creation and saving it to a var

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //alert("Welcome to the game!"); - in order to see if the JS <script></script> in index.html works
+
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber]; 
+    gamePattern.push(randomChosenColour);
+};
+
+
+nextSequence(); // The function does not need a return statement, since it pushes the data in the array.

f29212000e918ad4a391b8dbbae670bd5db8d9eb
Maria Tsioni <thalia.tsioni@gmail.com>
2023-10-25 15:14:10 +0300

    Added sound to the users button clicks.

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-//alert("Welcome to the game!"); - in order to see if the JS <script></script> in index.html works
 
-var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 var userClickedPattern = [];
 
+
+// -- This is the sequence that game itself creates when you enter the web page. -- //
+// -- Plus, flashing the selected button & playing the corresponding sound. -- //
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber]; 
     gamePattern.push(randomChosenColour);
 
     $("#"+randomChosenColour).fadeIn(160).fadeOut(160).fadeIn(160);    //Animating a flash to the choosen button.
-    $("#"+randomChosenColour).click(function(){
-        var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");     //Playing the sound for the button colour selected.
-        audio.play();
-    });
+    playSound(randomChosenColour);
 };
+nextSequence(); 
 
-nextSequence();   // The function does not need a return statement, since it pushes the data in the array.
 
-
+// -- Check which button is pressed by the user. -- //
 $(".btn").on("click", function () {
-    var userChosenColour = this.id;
-    userClickedPattern.push(userChosenColour);
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);  
+    playSound(userChosenColour);     
 });
+
+// -- Function playing the sound for the selected button, either by the user or by the program. -- //
+function playSound (name) {
+    var audio = new Audio("./sounds/" + name + ".mp3");     
+    audio.play();   
+};
+playSound();

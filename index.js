49903149e7967a94886284c3736b9ec5c5301d49
Maria Tsioni <thalia.tsioni@gmail.com>
2023-10-25 15:41:17 +0300

    Added Fading In/Out to user clicks.

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 
-// -- This is the sequence that game itself creates when you enter the web page. -- //
-// -- Plus, flashing the selected button & playing the corresponding sound. -- //
+// -- Check which button is pressed by the user. -- //
+$(".btn").on("click", function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);  
+
+    playSound(userChosenColour);     
+
+    animatePress(userChosenColour);
+});
+
+
+// -- This is the sequence that game itself creates when you enter the web page. Plus, flashing the selected button & playing the corresponding sound. -- //
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber]; 
@@ -13,20 +23,20 @@ function nextSequence() {
 
     $("#"+randomChosenColour).fadeIn(160).fadeOut(160).fadeIn(160);    //Animating a flash to the choosen button.
     playSound(randomChosenColour);
-};
-nextSequence(); 
+}
 
-
-// -- Check which button is pressed by the user. -- //
-$(".btn").on("click", function () {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);  
-    playSound(userChosenColour);     
-});
 
 // -- Function playing the sound for the selected button, either by the user or by the program. -- //
 function playSound (name) {
     var audio = new Audio("./sounds/" + name + ".mp3");     
     audio.play();   
-};
-playSound();
+}
+
+
+// -- Function animating a fade in and out, whenever the button is pressed by the user. -- //
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
